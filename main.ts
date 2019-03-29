d511088a004be92202bf07043fc0434bd4f89657
ian <ian.t.callaghan@gmail.com>
2019-03-29 09:38:39 +0000

    files

---
+++ main.ts
@@ -0,0 +1,33 @@
+import { Table, Column, Model } from "sequelize-typescript";
+
+@Table
+export class GamesDbImage extends Model<GamesDbImage> {
+  @Column({ primaryKey: true })
+  id: number;
+
+  @Column
+  type: string;
+
+  @Column
+  side: string;
+
+  @Column
+  filename: string;
+
+  @Column
+  resolution: string;
+}
+
+export class Main {
+  constructor() {
+
+  }
+  public async start() {      
+    const testOb = await GamesDbImage.findOne();
+    testOb.type;
+
+    GamesDbImage.findOne().then(testOb => {
+        testOb.side;
+    });
+  }
+}
